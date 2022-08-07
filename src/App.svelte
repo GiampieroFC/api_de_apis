c782e943683a6e7edb074e044acb618e848a47c8
GiampieroFC <GIAMPIEROFERMINICASTILLO@GMAIL.COM>
2022-08-08 00:49:14 +0300

    archivos limpiados para reescribir la api. Se instaló bulma

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+// const url = "https://api.publicapis.org/entries";
+
+
+// const cabeceras = {
+//   origin: '*',
+//   // mode: 'cors',
+//   // credentials: 'include',
+//   // headers: {'Content-Type' : 'aplication/json'}
+// };
+
+
+// fetch(url, cabeceras)
+// .then(response => response.json())
+// .then(data => api1 = data.entries)
+
+
+</script>
+
+<main>
+
+ 
+</main>
+
+<style>
+
+</style>
